8d2c91039537319b04988a1ed0ccaba292bb0d68
StephenGrider <ste.grider@gmail.com>
2014-01-24 23:26:50 -0800

    serving index html

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+
 var http = require('http');
 var path = require('path');
 
@@ -13,8 +13,8 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -26,7 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-app.get('/getItem', index.getItem);
+
+
+app.get('/', function(req,res) {
+  res.sendfile('public/index.html');
+});
+app.get('/getItem', routes.getItem);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
