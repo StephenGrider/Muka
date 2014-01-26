6df2e097723c39ba884d30e9cfee70e7af47c864
StephenGrider <ste.grider@gmail.com>
2014-01-26 15:42:31 -0800

    edit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // all environments
 
-app.set('port', process.env.PORT || 2999);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
