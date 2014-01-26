319210e5f17cbae4a52ad2ca0f2657be9e60bb36
StephenGrider <ste.grider@gmail.com>
2014-01-25 17:39:58 -0800

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,6 @@
 
 
-var items = [
-  {
-    itemImage: 'http://melissaeastondesign.com/blog/wp-content/uploads/2010/09/HGJones_Basket_01.jpg',
-    itemName: 'An Basket',
-    crafterImg: 'http://materialmetaphors.typepad.com/.a/6a011168663646970c01156fcae167970c-pi',
-    crafterBio: 'Long time resident of south pacific, lots of different crafts undertaken',
-    crafterName: 'Mr Crafter',
-    itemScore: 1,
-    itemPrice : '$39.00',
-    itemDescription: 'A long description about the item',
-    crafterUrl: '/innerpage/crafter1.html'
-  },
-  {
-    itemId: 2,
-    itemImage: 'http://content.vcommerce.com/products/667/57330667/fullsize.jpg',
-    itemName: 'A Fabric',
-    crafterImg: 'http://facultyblog.eternitybiblecollege.com/wp-content/uploads/2011/12/jesus_carpenter.jpg',
-    crafterBio: 'A bio about the crafter about all the different items they make and their particular situation',
-    crafterName: 'Mr Crafter',
-    itemScore: 1,
-    itemPrice : '$45.00',
-    itemDescription: 'Some cloth',
-    crafterUrl: '/innerpage/crafter2.html'
-  },
-  {
-    itemId: 3,
-    itemImage: 'http://jacobandlevis.com/CMS/uploads/Stafford_Queen_Bed.jpg',
-    itemName: 'A bed',
-    crafterImg: 'http://us.123rf.com/400wm/400/400/patrimonio/patrimonio1202/patrimonio120200073/12482221-illustration-of-a-carpenter-working-with-smooth-plane-done-in-retro-woodcut-style-set-inside-ellipse.jpg',
-    crafterBio: 'A bio about the crafter',
-    crafterName: 'Jeannette Builder',
-    itemScore: 1,
-    itemPrice : '$45.00',
-    itemDescription: 'sweet bed bro',
-    crafterUrl: '/innerpage/crafter3.html'
-  },
-  {
-    itemId: 4,
-    itemImage: 'http://www.cherricopottery.com/wp-content/uploads/2011/07/pottery-slides-woodfired1.jpg',
-    itemName: 'A neat piece of pottery',
-    crafterImg: 'http://materialmetaphors.typepad.com/.a/6a011168663646970c01156fcae167970c-pi',
-    crafterBio: 'For a long time i have made many different objects, looking to get more people interested in my prodcuts',
-    crafterName: 'Crafting Master',
-    itemScore: 1,
-    itemPrice : '$45.00',
-    itemDescription: 'Some awesome pottery',
-    crafterUrl: '/innerpage/crafter2.html'
-  }
-];
+
 
 
 exports.getItem = function(req, res){
@@ -71,5 +23,246 @@ exports.recordFeedback = function(req, res){
   res.end();
 }
 
+var items = [
+  {
+    itemImage: '/images/1.jpg',
+    itemName: 'Amaya Chair',
+    crafterImg: '/images/crafter1.jpg',
+    crafterBio: 'Mulakas is one the village elderly in the Mangyan tribe. He has 50 years of experience under his belt constructing furniture made of bamboo and rattan...',
+    crafterName: 'Mulakas',
+    itemScore: 1,
+    itemPrice : '$39.00',
+    itemDescription: 'It’s the weekend, and as far as you’re concerned, it’s time for the living to get easy. Whether it’s the Eastern seaboard or Cali coast you’re itching for, indulge in the tropical colors this product has to offer. This rattan armchair will bring your fresh vibe and keen wit to the forefront with freewheeling summer style.',
+    crafterUrl: '/innerpage/crafter1.html'
+  },
+  {
+    itemId: 2,
+    itemImage: '/images/2.jpg',
+    itemName: 'Antique',
+    crafterImg: '/images/crafter1.jpg',
+    rafterBio: 'Mulakas is one the village elderly in the Mangyan tribe. He has 50 years of experience under his belt constructing furniture made of bamboo and rattan...',
+    crafterName: 'Mulakas',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'It’s the weekend, and as far as you’re concerned, it’s time for the living to get easy. Whether it’s the Eastern seaboard or Cali coast you’re itching for, indulge in the tropical colors this product has to offer. This rattan armchair will bring your fresh vibe and keen wit to the forefront with freewheeling summer style.',
+    crafterUrl: '/innerpage/crafter1.html'
+  },
+  {
+    itemId: 3,
+    itemImage: '/images/3.jpg',
+    itemName: 'Bumble',
+    crafterImg: '/images/crafter1.jpg',
+    rafterBio: 'Mulakas is one the village elderly in the Mangyan tribe. He has 50 years of experience under his belt constructing furniture made of bamboo and rattan...',
+    crafterName: 'Mulakas',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'It’s the weekend, and as far as you’re concerned, it’s time for the living to get easy. Whether it’s the Eastern seaboard or Cali coast you’re itching for, indulge in the tropical colors this product has to offer. This rattan armchair will bring your fresh vibe and keen wit to the forefront with freewheeling summer style.',
+    crafterUrl: '/innerpage/crafter1.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/4.jpg',
+    itemName: 'Cubbyhole',
+    crafterImg: '/images/crafter1.jpg',
+    rafterBio: 'Mulakas is one the village elderly in the Mangyan tribe. He has 50 years of experience under his belt constructing furniture made of bamboo and rattan...',
+    crafterName: 'Mulakas',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'It’s the weekend, and as far as you’re concerned, it’s time for the living to get easy. Whether it’s the Eastern seaboard or Cali coast you’re itching for, indulge in the tropical colors this product has to offer. This rattan armchair will bring your fresh vibe and keen wit to the forefront with freewheeling summer style.',
+    crafterUrl: '/innerpage/crafter1.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/5.jpg',
+    itemName: 'Dividend',
+    crafterImg: '/images/crafter1.jpg',
+    rafterBio: 'Mulakas is one the village elderly in the Mangyan tribe. He has 50 years of experience under his belt constructing furniture made of bamboo and rattan...',
+    crafterName: 'Mulakas',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'It’s the weekend, and as far as you’re concerned, it’s time for the living to get easy. Whether it’s the Eastern seaboard or Cali coast you’re itching for, indulge in the tropical colors this product has to offer. This rattan armchair will bring your fresh vibe and keen wit to the forefront with freewheeling summer style.',
+    crafterUrl: '/innerpage/crafter1.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/6.jpg',
+    itemName: 'La Luna',
+    crafterImg: '/images/crafter1.jpg',
+    rafterBio: 'Mulakas is one the village elderly in the Mangyan tribe. He has 50 years of experience under his belt constructing furniture made of bamboo and rattan...',
+    crafterName: 'Mulakas',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'It’s the weekend, and as far as you’re concerned, it’s time for the living to get easy. Whether it’s the Eastern seaboard or Cali coast you’re itching for, indulge in the tropical colors this product has to offer. This rattan armchair will bring your fresh vibe and keen wit to the forefront with freewheeling summer style.',
+    crafterUrl: '/innerpage/crafter1.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/7.jpg',
+    itemName: 'Lemongrass',
+    crafterImg: '/images/crafter1.jpg',
+    rafterBio: 'Mulakas is one the village elderly in the Mangyan tribe. He has 50 years of experience under his belt constructing furniture made of bamboo and rattan...',
+    crafterName: 'Mulakas',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'It’s the weekend, and as far as you’re concerned, it’s time for the living to get easy. Whether it’s the Eastern seaboard or Cali coast you’re itching for, indulge in the tropical colors this product has to offer. This rattan armchair will bring your fresh vibe and keen wit to the forefront with freewheeling summer style.',
+    crafterUrl: '/innerpage/crafter1.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/8.jpg',
+    itemName: 'Lightbox',
+    crafterImg: '/images/crafter1.jpg',
+    rafterBio: 'Mulakas is one the village elderly in the Mangyan tribe. He has 50 years of experience under his belt constructing furniture made of bamboo and rattan...',
+    crafterName: 'Mulakas',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'It’s the weekend, and as far as you’re concerned, it’s time for the living to get easy. Whether it’s the Eastern seaboard or Cali coast you’re itching for, indulge in the tropical colors this product has to offer. This rattan armchair will bring your fresh vibe and keen wit to the forefront with freewheeling summer style.',
+    crafterUrl: '/innerpage/crafter1.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/9.jpg',
+    itemName: 'Manolo',
+    crafterImg: '/images/crafter1.jpg',
+    rafterBio: 'Mulakas is one the village elderly in the Mangyan tribe. He has 50 years of experience under his belt constructing furniture made of bamboo and rattan...',
+    crafterName: 'Mulakas',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'It’s the weekend, and as far as you’re concerned, it’s time for the living to get easy. Whether it’s the Eastern seaboard or Cali coast you’re itching for, indulge in the tropical colors this product has to offer. This rattan armchair will bring your fresh vibe and keen wit to the forefront with freewheeling summer style.',
+    crafterUrl: '/innerpage/crafter1.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/10.jpg',
+    itemName: 'Marrow',
+    crafterImg: '/images/crafter2.jpg',
+    crafterBio: 'At the age of 18, Tala is already a master of weaving natural fibers in to one of a kind furniture pieces. She enjoys creating colorful patterns with her close knit Aeta community...',
+    crafterName: 'Tala',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Streamlined in its aesthetic, bumble lounge is equal parts awesome-and-modest. This uniquely beautiful realm of natural hand woven rattan is handcrafted in Cebu, Philippines, and features natural patterns.',
+    crafterUrl: '/innerpage/crafter2.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/11.jpg',
+    itemName: 'Mars',
+    crafterImg: '/images/crafter2.jpg',
+    crafterBio: 'At the age of 18, Tala is already a master of weaving natural fibers in to one of a kind furniture pieces. She enjoys creating colorful patterns with her close knit Aeta community...',
+    crafterName: 'Tala',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Streamlined in its aesthetic, bumble lounge is equal parts awesome-and-modest. This uniquely beautiful realm of natural hand woven rattan is handcrafted in Cebu, Philippines, and features natural patterns.',
+    crafterUrl: '/innerpage/crafter2.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/12.jpg',
+    itemName: 'Matchbox',
+    crafterImg: '/images/crafter2.jpg',
+    crafterBio: 'At the age of 18, Tala is already a master of weaving natural fibers in to one of a kind furniture pieces. She enjoys creating colorful patterns with her close knit Aeta community...',
+    crafterName: 'Tala',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Streamlined in its aesthetic, bumble lounge is equal parts awesome-and-modest. This uniquely beautiful realm of natural hand woven rattan is handcrafted in Cebu, Philippines, and features natural patterns.',
+    crafterUrl: '/innerpage/crafter2.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/13.jpg',
+    itemName: 'Oasis',
+    crafterImg: '/images/crafter2.jpg',
+    crafterBio: 'At the age of 18, Tala is already a master of weaving natural fibers in to one of a kind furniture pieces. She enjoys creating colorful patterns with her close knit Aeta community...',
+    crafterName: 'Tala',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Streamlined in its aesthetic, bumble lounge is equal parts awesome-and-modest. This uniquely beautiful realm of natural hand woven rattan is handcrafted in Cebu, Philippines, and features natural patterns.',
+    crafterUrl: '/innerpage/crafter2.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/14.jpg',
+    itemName: 'Steady Leaf',
+    crafterImg: '/images/crafter2.jpg',
+    crafterBio: 'At the age of 18, Tala is already a master of weaving natural fibers in to one of a kind furniture pieces. She enjoys creating colorful patterns with her close knit Aeta community...',
+    crafterName: 'Tala',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Streamlined in its aesthetic, bumble lounge is equal parts awesome-and-modest. This uniquely beautiful realm of natural hand woven rattan is handcrafted in Cebu, Philippines, and features natural patterns.',
+    crafterUrl: '/innerpage/crafter2.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/15.jpg',
+    itemName: 'Sunset',
+    crafterImg: '/images/crafter2.jpg',
+    crafterBio: 'At the age of 18, Tala is already a master of weaving natural fibers in to one of a kind furniture pieces. She enjoys creating colorful patterns with her close knit Aeta community...',
+    crafterName: 'Tala',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Streamlined in its aesthetic, bumble lounge is equal parts awesome-and-modest. This uniquely beautiful realm of natural hand woven rattan is handcrafted in Cebu, Philippines, and features natural patterns.',
+    crafterUrl: '/innerpage/crafter2.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/16.jpg',
+    itemName: 'The Ladybug',
+    crafterImg: '/images/crafter3.jpg',
+    crafterBio: 'Representing the strong end of the community, Alibatas masculinity is exhibited in his furniture. A strong sense of pride and integrity motivates him to bring income to the remote Waray ethnic group in Southern Leyte...',
+    crafterName: 'Alibata',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Minimalist to the max, this side table proves the power of simplicity.The choice of earthy materials and pull towards stripped-down beauty results in a clean design that’s also a bit bohemian, and at times edgy.',
+    crafterUrl: '/innerpage/crafter3.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/17.jpg',
+    itemName: 'Tropez',
+    crafterImg: '/images/crafter3.jpg',
+    crafterBio: 'Representing the strong end of the community, Alibatas masculinity is exhibited in his furniture. A strong sense of pride and integrity motivates him to bring income to the remote Waray ethnic group in Southern Leyte...',
+    crafterName: 'Alibata',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Minimalist to the max, this side table proves the power of simplicity.The choice of earthy materials and pull towards stripped-down beauty results in a clean design that’s also a bit bohemian, and at times edgy.',
+    crafterUrl: '/innerpage/crafter3.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/18.jpg',
+    itemName: 'Wings',
+    crafterImg: '/images/crafter3.jpg',
+    crafterBio: 'Representing the strong end of the community, Alibatas masculinity is exhibited in his furniture. A strong sense of pride and integrity motivates him to bring income to the remote Waray ethnic group in Southern Leyte...',
+    crafterName: 'Alibata',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Minimalist to the max, this side table proves the power of simplicity.The choice of earthy materials and pull towards stripped-down beauty results in a clean design that’s also a bit bohemian, and at times edgy.',
+    crafterUrl: '/innerpage/crafter3.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/19.jpg',
+    itemName: 'Zen',
+    crafterImg: '/images/crafter3.jpg',
+    crafterBio: 'Representing the strong end of the community, Alibatas masculinity is exhibited in his furniture. A strong sense of pride and integrity motivates him to bring income to the remote Waray ethnic group in Southern Leyte...',
+    crafterName: 'Alibata',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Minimalist to the max, this side table proves the power of simplicity.The choice of earthy materials and pull towards stripped-down beauty results in a clean design that’s also a bit bohemian, and at times edgy.',
+    crafterUrl: '/innerpage/crafter3.html'
+  },
+  {
+    itemId: 4,
+    itemImage: '/images/20.jpg',
+    itemName: 'Ziggy',
+    crafterImg: '/images/crafter3.jpg',
+    crafterBio: 'Representing the strong end of the community, Alibatas masculinity is exhibited in his furniture. A strong sense of pride and integrity motivates him to bring income to the remote Waray ethnic group in Southern Leyte...',
+    crafterName: 'Alibata',
+    itemScore: 1,
+    itemPrice : '$45.00',
+    itemDescription: 'Minimalist to the max, this side table proves the power of simplicity.The choice of earthy materials and pull towards stripped-down beauty results in a clean design that’s also a bit bohemian, and at times edgy.',
+    crafterUrl: '/innerpage/crafter3.html'
+  }
 
+];
 
